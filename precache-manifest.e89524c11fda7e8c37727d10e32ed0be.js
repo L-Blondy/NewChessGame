2c853df88941242c01da3344bd7a2427e7cabeb4
Laurent Blondy <laurent.blondy1@gmail.com>
2019-10-10 11:43:58 +0200

    Updates

---
+++ precache-manifest.e89524c11fda7e8c37727d10e32ed0be.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e89704eb4ad043770c3792b024a25cf9",
+    "url": "/NewChessGame/index.html"
+  },
+  {
+    "revision": "cc68c773b7fca7477bdb",
+    "url": "/NewChessGame/static/css/main.9e44a2fa.chunk.css"
+  },
+  {
+    "revision": "4b177e2ff0746c36c5c6",
+    "url": "/NewChessGame/static/js/2.589eb858.chunk.js"
+  },
+  {
+    "revision": "cc68c773b7fca7477bdb",
+    "url": "/NewChessGame/static/js/main.f2d4de42.chunk.js"
+  },
+  {
+    "revision": "d09eda2b3bb46132e349",
+    "url": "/NewChessGame/static/js/runtime~main.e87011cd.js"
+  },
+  {
+    "revision": "0fbeae9e9356b51dfb28c8d03fd65663",
+    "url": "/NewChessGame/static/media/blackbishop.0fbeae9e.svg"
+  },
+  {
+    "revision": "55996645cda27f3f1631a6875d4c87e4",
+    "url": "/NewChessGame/static/media/blackking.55996645.svg"
+  },
+  {
+    "revision": "9448fe651f3f737e9da72773a3e85ecb",
+    "url": "/NewChessGame/static/media/blackknight.9448fe65.svg"
+  },
+  {
+    "revision": "1936b4fb84c15eb04dc690135acb7ae8",
+    "url": "/NewChessGame/static/media/blackpawn.1936b4fb.svg"
+  },
+  {
+    "revision": "1e08c1cd81452f76c0b494fb9e593333",
+    "url": "/NewChessGame/static/media/blackqueen.1e08c1cd.svg"
+  },
+  {
+    "revision": "de028d4c561a659003bd6ada16d96c45",
+    "url": "/NewChessGame/static/media/blackrook.de028d4c.svg"
+  },
+  {
+    "revision": "247f2288a49d4b4d33b06e5b74d5bf00",
+    "url": "/NewChessGame/static/media/whitebishop.247f2288.svg"
+  },
+  {
+    "revision": "d48cb8c691921dcf4b57c4795d439a44",
+    "url": "/NewChessGame/static/media/whiteking.d48cb8c6.svg"
+  },
+  {
+    "revision": "d9e7ee7d292830ffdc7a7dbda00c4694",
+    "url": "/NewChessGame/static/media/whiteknight.d9e7ee7d.svg"
+  },
+  {
+    "revision": "e3ae362d6c3a2139ded8f9394fde31be",
+    "url": "/NewChessGame/static/media/whitepawn.e3ae362d.svg"
+  },
+  {
+    "revision": "899af5812c7ae95b41a3f62aa65bb3be",
+    "url": "/NewChessGame/static/media/whitequeen.899af581.svg"
+  },
+  {
+    "revision": "2775cbc717f657a4e2b6ce37587593f6",
+    "url": "/NewChessGame/static/media/whiterook.2775cbc7.svg"
+  }
+]);
